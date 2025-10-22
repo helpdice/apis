da5bbea53b3d7afbda6097540fbc9a7941dad8d3
helpdice <packer@helpdice.com>
2025-10-22 07:15:12 +0530

    fixed cors issues

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const env = require('./config/environment');
 require('./config/mongoose');
 const app = express();
 // const PORT = env.port;
-
-app.use(cors());
+app.use(cors({
+    origin: ['https://ui.helpdice.com', 'http://localhost:3000'], // frontend URLs
+    credentials: true
+}));
 
 // Setup the location server to be used with socket.io
 // const server = http.createServer(app);
